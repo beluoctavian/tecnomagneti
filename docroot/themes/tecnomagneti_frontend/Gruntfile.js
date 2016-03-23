322f37f5eaa30c31c1b13d5f15d3559e14a77a4b
Belu Octavian <belu.octavian@yahoo.com>
2016-03-23 16:33:52 +0200

    Initialized grunt

---
+++ docroot/themes/tecnomagneti_frontend/Gruntfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "css/style.css": "less/style.less" // destination file and source file
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['less/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['less', 'watch']);
+};
