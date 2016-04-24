a0dc2924a1e2e4bc4fccc024b1da71b6bb1c1870
Belu Octavian <belu.octavian@yahoo.com>
2016-04-24 18:17:52 +0300

    News bar style

--- docroot/themes/tecnomagneti_frontend/assets/js/application.js
+++ docroot/themes/tecnomagneti_frontend/assets/js/application.js
@@ -4,4 +4,10 @@
             top: $('header').height(),
         }
     });
+    $('#block-views-block-news-block-1 .item-list ul').ticker({
+        controls: false, //show controls, to be implemented
+        interval: 5000, //interval to show next item
+        effect: "slideUp", // available effects: fadeIn, slideUp, slideDown
+        duration: 400 //duration of the change to the next item
+    });
 })(jQuery);
